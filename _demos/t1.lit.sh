79492b4654f760ca85e5ae92002e6f2bc7ea9af4
Allen Guo <guosonglin001@gmail.com>
2024-05-02 19:27:45 +0800

    wip

--- _demos/t1.lit.sh
+++ _demos/t1.lit.sh
@@ -17,6 +17,17 @@ args=(
   # --debug
   -v -a
   # --filter "MLIR_HLO_OPT :: bufferize.mlir"
-  --filter "MLIR_HLO_OPT :: bufferize_one_shot.mlir"
+  # --filter "MLIR_HLO_OPT :: bufferize_one_shot.mlir"
+  --filter "Dialect/deallocation/buffer_reuse.mlir"
 )
-lit "${args[@]}" | tee _demos/lit.run.dirty.mlir
+lit "${args[@]}"
+# lit "${args[@]}" | tee _demos/lit.run.dirty.mlir
+
+###############################################################################
+
+export FILECHECK_OPTS="--vv --dump-input=always --color=1"
+
+mlir-hlo-opt tests/Dialect/deallocation/buffer_reuse.mlir -split-input-file -allow-unregistered-dialect -hlo-buffer-reuse |
+  FileCheck tests/Dialect/deallocation/buffer_reuse.mlir
+
+###############################################################################
